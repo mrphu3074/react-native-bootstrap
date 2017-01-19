5d1b5ad05286a704cc6514e4a1f18f8217b36af4
Phu Nguyen <mrphu3074@gmail.com>
2017-01-19 14:37:24 +0700

    integrated firebase

---
+++ ios/Pods/Target Support Files/Pods-RNBootstrap/Pods-RNBootstrap-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNBootstrap : NSObject
+@end
+@implementation PodsDummy_Pods_RNBootstrap
+@end
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,26 +2,59 @@ import React from 'react';
 import {
   View,
   Text,
-  Button
+  Button,
+  ScrollView,
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { LoginButton, AppEventsLogger } from 'react-native-fbsdk';
+import _ from 'lodash';
 
+import Firestack from 'react-native-firestack'
 
-export default () => (
-  <View style={styles.container}>
-    <Text>HomeScreen</Text>
-    <Icon name={'ios-add'} size={40} />
-    <Button title="Go PushScreen" onPress={() => {
-      Actions.pushScreen()
-    }} />
-  </View>
-);
+const firestack = new Firestack({
+  persistence: true
+});
+
+const todoRef = firestack.database.ref('todos');
+todoRef.keepSynced(true);
+
+export default class HomeScreen extends React.Component {
+  state = {
+    items: []
+  };
+  componentDidMount() {
+    todoRef.on('value', snapshot => {
+      this.setState({
+        items: _.toArray(snapshot.val())
+      });
+    })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>HomeScreen</Text>
+        <Button title="Push Screen" onPress={() => {
+          Actions.pushScreen();
+        }}/>
+        <LoginButton />
+
+        <ScrollView>
+          {this.state.items.map((item, key) => (
+            <View key={key}>
+              <Text style={{ color: '#666' }}>{item.text}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
 const styles = {
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#BBDEFB',
+    paddingTop: 64
   }
 };
